8d6ceae5f9684d7898b8f6872e9ac5c3c3a40f2a
Fazil Chengapra <muhammedfazilchengapra@gmail.com>
2025-06-22 19:36:11 +0530

    react version update to 19 to 18 not supported many libraries

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,21 +1,28 @@
-import { Facebook, Github, Twitter } from 'lucide-react'
-import React from 'react'
+import React from "react";
+import { FaFacebookF, FaGithub } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className='rounded-md w-full bg-white py-6'>
-        <div className='grid grid-cols-10 mx-5 items-center'>
-            <div className='text-[#6B7280] text-xs font-semibold col-span-9'>@ 2025 Shoppee, All rights reserved.</div>
-            <div className='col-span-1 grid grid-cols-3 gap-4'>
-                <div><Facebook /></div>
-                <div><Twitter  /></div>
-                <div><Github  /></div>
-                
-            </div>
-            
+    <div className="rounded-md w-full bg-white py-6">
+      <div className="grid grid-cols-10 mx-5 items-center">
+        <div className="text-[#6B7280] text-xs font-semibold col-span-9">
+          @ 2025 Shoppee, All rights reserved.
         </div>
+        <div className="col-span-1 grid grid-cols-3 gap-4">
+          <div>
+            <FaFacebookF />
+          </div>
+          <div>
+            <FaTwitter />
+          </div>
+          <div>
+            <FaGithub />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
